2686f4f2cd907062968eb10e5d33f06ad366d454
DouglasFuelber <contato@douglasfuelber.com>
2020-04-01 16:06:18 -0300

    Added blackbox tests to math class

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -7,12 +7,43 @@ namespace TestNinja.UnitTests
     class MathTests
     {
         [Test]
-        public void Add_WhenCalled_ReturnSumOfArguments() {
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
             var math = new Math();
 
             var result = math.Add(1, 2);
 
             Assert.AreEqual(result, 3);
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(2, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnSameArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 1);
+
+            Assert.That(result, Is.EqualTo(1));
         }
     }
 }
