d29d013d63199c812b7853fe408c92932c1117f0
DouglasFuelber <contato@douglasfuelber.com>
2020-04-01 15:59:18 -0300

    Created simple unit test

---
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments() {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.AreEqual(result, 3);
+        }
+    }
+}
