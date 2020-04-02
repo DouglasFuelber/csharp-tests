c849a0c451b5938522bb6249c07f877bf06bdc1e
DouglasFuelber <contato@douglasfuelber.com>
2020-04-02 14:37:14 -0300

    Added arrays and collections tests

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Linq;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -20,7 +21,7 @@ namespace TestNinja.UnitTests
         {
             var result = _math.Add(1, 2);
 
-            Assert.AreEqual(result, 3);
+            Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
@@ -32,6 +33,24 @@ namespace TestNinja.UnitTests
             var result = _math.Max(a, b);
 
             Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result.Count(), Is.EqualTo(3));
+
+            Assert.That(result, Does.Contain(1));
+            Assert.That(result, Does.Contain(3));
+            Assert.That(result, Does.Contain(5));
+
+            Assert.That(result, Is.EquivalentTo(new[] { 1, 3, 5 }));
+
+            Assert.That(result, Is.Ordered);
+            Assert.That(result, Is.Unique);
         }
     }
 }
